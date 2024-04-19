5b7e52fec588f0a945cec8d2ff1aed5653289158
Michael Krebs <michikrbs@gmail.com>
2024-04-19 22:34:22 +0200

    Update category

--- src/routes/leistungen/Category.svelte
+++ src/routes/leistungen/Category.svelte
@@ -6,7 +6,7 @@
   export let toggleService: any; // Ensure toggleService is declared as a prop
 </script>
 
-<div class="flex flex-col gap-4">
+<div class="flex flex-row max-w-2/5 flex-wrap gap-10">
   {#each category.services as service}
     <Service {service} {activeServiceId} {toggleService} />
   {/each}
