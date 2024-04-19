9febe7b6a903cbb0d3d4dc0efa9db4060e97be86
Ralf Pauli <ralf.pauli15@gmail.com>
2024-04-19 17:25:28 +0200

    add 404-, impressum- and datenschutz-page

--- src/routes/kontakt/+page.svelte
+++ src/routes/kontakt/+page.svelte
@@ -3,11 +3,19 @@
   import { PUBLIC_GOOGLE_MAPS_API_KEY, PUBLIC_PLACE_ID, PUBLIC_STRAPI_HOST } from "$env/static/public";
   import placeholderImage from "$lib/assets/31.jpg";
   import { createLocalStorage } from "$lib/shared/stores/local-storage";
+  import { onMount } from "svelte";
+  import { Spinner } from 'flowbite-svelte';
 
   export let data: PageData;
   let contactPage = data.contact_page;
   let loadGoogleMap = createLocalStorage<boolean>("load-google-map", false);
-  let shouldLoad = false;
+  let shouldLoad: boolean | null = false;
+  let isLoading = true;
+
+  onMount(() => {
+    shouldLoad = loadGoogleMap.get();
+    isLoading = false;
+  });
 
   function handleAlwaysLoadChange(event: Event) {
     const target = event.target as HTMLInputElement;
@@ -18,7 +26,11 @@
 
 <!-- Map Section -->
 <div class="map-container w-3/5 h-2/5 block relative mx-auto">
-  {#if shouldLoad || loadGoogleMap.get()}
+  {#if isLoading}
+    <div class="w-full h-full bg-secondary inset-0 flex justify-center items-center">
+      <Spinner class="text-center fill-primary absolute"/>
+    </div>
+  {:else if loadGoogleMap.get() || shouldLoad}
     <iframe
       title="Google Map"
       allowfullscreen
@@ -28,17 +40,25 @@
       style="border:0">
     </iframe>
   {:else}
-    <img class="w-full h-full object-cover" src={placeholderImage} alt="Placeholder Map Image">
-    <div class="absolute inset-0 flex flex-col items-center justify-center text-white bg-black bg-opacity-50">
-      <p class="text-center mb-4">Mit dem Laden der Karte akzeptieren Sie die Datenschutzerklärung von Google.</p>
-      <div class="flex flex-col items-center">
-        <button class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-2"
-                on:click={() => shouldLoad = true}>Karte laden
-        </button>
-        <label class="flex items-center text-sm">
-          <input type="checkbox" class="mr-2" on:change="{handleAlwaysLoadChange}" checked="{loadGoogleMap.get()}">
-          <span>Google Maps immer entsperren</span>
-        </label>
+    <img class="w-full h-full object-cover" src={placeholderImage} alt="">
+
+    <div class="absolute left-0 right-0 top-1/2 transform -translate-y-1/2 bg-black bg-opacity-50">
+      <div class="text-center text-white p-4">
+        <p>Mit dem Laden der Karte akzeptieren Sie die Datenschutzerklärung von Google.<br>
+          <a href="https://policies.google.com/privacy" class="text-blue-300 hover:text-blue-500"
+             rel="nofollow noopener noreferrer" target="_blank">Mehr erfahren</a>
+        </p>
+        <p class="my-2">
+          <button class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                  on:click={() => shouldLoad = true}>Karte laden
+          </button>
+        </p>
+        <div class="flex justify-center">
+          <label class="flex items-center">
+            <input type="checkbox" class="mr-2" on:change="{handleAlwaysLoadChange}" checked="{loadGoogleMap.get()}">
+            <small>Google Maps immer entsperren</small>
+          </label>
+        </div>
       </div>
     </div>
   {/if}
@@ -68,11 +88,10 @@
   </div>
 </div>
 
-<!-- Social Media Links -->
-<div class="social-media flex justify-center gap-4">
-  {#each contactPage.social_media as social}
-    <a href="{social.url}" title="{social.name}" target="_blank" rel="noopener noreferrer">
-      <img src={`${PUBLIC_STRAPI_HOST}${social.icon.formats.thumbnail.url}`} alt="{social.name}" class="h-8">
-    </a>
+<div>
+  {#each contactPage.social_media as social_media}
+    <a href="{social_media.url}">
+      <img src="{PUBLIC_STRAPI_HOST}{social_media.icon.formats.thumbnail.url}" class="object-contain h-[30px]"
+           alt="{social_media.name}"></a>
   {/each}
 </div>
