c4593b2d5015e711d43b331775e29deda1e0cd10
Michael Krebs <michikrbs@gmail.com>
2024-05-01 00:31:11 +0200

    ui: refactor spinner size

--- src/routes/leistungen/+page.svelte
+++ src/routes/leistungen/+page.svelte
@@ -54,7 +54,7 @@
   <div class="flex gap-3 pt-3 w-full">
     {#key activeCategoryId}
       {#if activeCategoryId === null}
-        <div class="m-auto w-16 h-16 border-8 border-dashed rounded-full animate-spin border-gray-500" />
+        <div class="m-auto w-8 h-8 border-4 border-dashed rounded-full animate-spin border-t-primary" />
       {:else}
         <Category category={getActiveCategory} {activeServiceId} {toggleService} />
       {/if}
