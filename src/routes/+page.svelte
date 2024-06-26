78e42fed89d8f7602a798a60729682ea0bac73d6
Michael Krebs <michikrbs@gmail.com>
2024-06-27 00:06:53 +0200

    fix: cleanup page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,11 @@
 <script lang="ts">
-  import type { PageData } from "./$types";
   import { Button } from "$components/ui/button";
-  import heroImage from "$lib/assets/hero.jpg";
-
-  export let data: PageData;
 </script>
 
 <div class="flex flex-col items-stretch justify-center gap-4 h-4/5 max-w-5xl mx-auto font-montserrat">
   <!-- Hero Section -->
   <!-- svelte-ignore a11y-img-redundant-alt -->
-  <img src={heroImage} alt="Hero Image" class="object-cover rounded-xl absolute max-w-5xl opacity-10" />
+  <enhance:img src="$lib/assets/hero.jpg" alt="Hero Image" class="object-cover rounded-xl absolute max-w-5xl opacity-10" />
   <div class="relative h-5/6 text-white">
     <div class="flex flex-col items-center justify-center h-full bg-opacity-10 p-4 rounded-xl relative">
       <h1 class="text-4xl font-bold inline border-b-primary mb-2 underline decoration-primary decoration-4 underline-offset-4">Willkommen bei Haarstudio Pauli</h1>
