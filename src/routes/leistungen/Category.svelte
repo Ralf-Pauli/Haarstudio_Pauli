0b1d0f2ecece42a82a57c536205f6460df63e8e8
Michael Krebs <michikrbs@gmail.com>
2024-09-30 21:14:23 +0200

    feat: visual rework of services
    - table like background on every 2nd
    - grouping of other services

--- src/routes/leistungen/Category.svelte
+++ src/routes/leistungen/Category.svelte
@@ -4,14 +4,69 @@
   export let category: any;
   export let activeServiceId: any;
   export let toggleService: any; // Ensure toggleService is declared as a prop
+
+  let serviceWithSubServices = category.services.filter((service: any) => service.sub_services.length > 0);
+  let otherServices = category.services.filter((service: any) => service.sub_services.length === 0);
+
+  let formatPrice = (price: any) => {
+    return new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "EUR",
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    }).format(price);
+  };
 </script>
 
 <div class="flex w-full flex-wrap">
   {#if category.services.length == 0}
     <div class="pl-6 text-center">No services available</div>
-  {:else}
-    {#each category.services as service}
+  {:else if serviceWithSubServices.length > 0}
+    {#each serviceWithSubServices as service}
       <Service {service} {activeServiceId} {toggleService} />
     {/each}
+    {#if otherServices.length > 0}
+      <div class="w-1/2 px-6 pb-10">
+        <div class="flex gap-2 items-center">
+          <div class="inline font-bold border-b-primary pb-1 mb-2 underline decoration-primary decoration-2 underline-offset-4">Sonstige Leistungen</div>
+        </div>
+        <div class="flex flex-col gap-1 ml-6 mt-3">
+          {#each otherServices as service, index}
+            {#if index % 2 === 1 && index > 0}
+              <div class="flex bg-zinc-800 p-2 rounded-md">
+                <div>{service.name}</div>
+                <div class="font-bold ml-auto">{formatPrice(service.price)}</div>
+              </div>
+            {:else}
+              <div class="flex p-2">
+                <div>{service.name}</div>
+                <div class="font-bold ml-auto">{formatPrice(service.price)}</div>
+              </div>
+            {/if}
+          {/each}
+        </div>
+      </div>
+    {/if}
+  {:else}
+    <div class="w-1/2 px-6 pb-10">
+      <div class="flex gap-2 items-center">
+        <div class="inline font-bold border-b-primary pb-1 mb-2 underline decoration-primary decoration-2 underline-offset-4">{category.name}</div>
+      </div>
+      <div class="flex flex-col gap-1 ml-6 mt-3">
+        {#each category.services as service, index}
+          {#if index % 2 === 1 && index > 0}
+            <div class="flex bg-zinc-800 p-2 rounded-md">
+              <div>{service.name}</div>
+              <div class="font-bold ml-auto">{formatPrice(service.price)}</div>
+            </div>
+          {:else}
+            <div class="flex p-2">
+              <div>{service.name}</div>
+              <div class="font-bold ml-auto">{formatPrice(service.price)}</div>
+            </div>
+          {/if}
+        {/each}
+      </div>
+    </div>
   {/if}
 </div>
