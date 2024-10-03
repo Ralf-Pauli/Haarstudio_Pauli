46c8ae3c3eb77bee581b457b1c41ab45ac4913eb
Michael Krebs <michikrbs@gmail.com>
2024-10-04 01:20:30 +0200

    fix footer layout

--- src/lib/components/footer/Footer.svelte
+++ src/lib/components/footer/Footer.svelte
@@ -10,21 +10,19 @@
   let socialMediaPromise = getSocialMedia();
 </script>
 
-<footer class="w-full flex gap-16 mx-auto justify-between pt-4 inset-x-0 h-16 max-w-5xl sticky top-[100vh] text-sm">
+<footer class="w-full grid grid-cols-3 mx-auto pt-4 inset-x-0 h-16 max-w-5xl sticky top-[100vh] text-sm">
   <!--  TODO: use correct name -->
-  <p>&copy; {new Date().getFullYear()} Haarstudio Pauli</p>
-  <div>
+  <p class="flex-shrink-0">&copy; {new Date().getFullYear()} Haarstudio Pauli</p>
+  <div class="mx-auto">
     {#await socialMediaPromise then socialMedia}
       {#each socialMedia as item}
         <a href={item.url} target="_blank">
-          <img src={PUBLIC_STRAPI_HOST + item.icon.formats.small.url} alt={item.name} class="inline mb-1 w-[16px] h-[16px] mr-[8px]" />
+          <img src={PUBLIC_STRAPI_HOST + item.icon.formats.small.url} alt={item.name} class="inline w-[16px] h-[16px] mx-1" />
         </a>
       {/each}
-    {:catch error}
-      {error.message}
     {/await}
   </div>
-  <div>
+  <div class="ml-auto">
     <a href="/impressum">Impressum</a>
     <span>|</span>
     <a href="/datenschutz">Datenschutzerklärung</a>
