1c0a6abaf0e030db4b8f108df21bac3e15468236
Michael Krebs <michikrbs@gmail.com>
2024-12-10 22:29:57 +0100

    refactor: visual tab marking

--- src/routes/leistungen/Tab.svelte
+++ src/routes/leistungen/Tab.svelte
@@ -3,6 +3,6 @@
   export let setActiveCategory: Function;
 </script>
 
-<button class="m-2 p-1 pl-2 w-full text-left border-solid border-t-2 md:border-t-0 md:border-l-2 border-transparent whitespace-nowrap" on:click={setActiveCategory(category.id)}>
+<button class="m-2 p-1 pl-2 w-full text-left border-solid border-b-2 md:border-b-0 md:border-l-2 border-transparent whitespace-nowrap" on:click={setActiveCategory(category.id)}>
   {category.name}
 </button>
