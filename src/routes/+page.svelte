ec9a25655ab8bb12f93502bb398c14e93204e4b1
Ralf Pauli <ralf.pauli15@gmail.com>
2024-12-10 21:45:57 +0100

    Update category cards to use strapi images

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,17 +2,39 @@
   import type { PageData } from "./$types";
   import BookingWidget from "$components/BookingWidget.svelte";
   import { Button } from "$components/ui/button";
+  import { PUBLIC_STRAPI_HOST } from "$env/static/public";
+
+  // Constants
+  const COLUMNS_PER_ROW = 3;
 
   export let data: PageData;
-  let { categories } = data;
 
-  const columnsPerRow = 3;
-  const categoryRows: any[] = [];
-  for (let i = 0; i < categories.length; i += columnsPerRow) {
-    categoryRows.push(categories.slice(i, i + columnsPerRow));
+  let categoryList = updateCategoryImages(data.categories);
+  let categoryRows = chunkCategoriesIntoRows(categoryList, COLUMNS_PER_ROW);
+
+  // Function to update category images with full URLs
+  function updateCategoryImages(categories: Array<{ image?: { url: string } }>) {
+    return categories.map(category => {
+      const imageUrl = category.image?.url;
+
+      if (imageUrl) {
+        const fullUrl = imageUrl.startsWith("http") ? imageUrl : `${PUBLIC_STRAPI_HOST}${imageUrl}`;
+        return { ...category, image: { ...category.image, url: fullUrl } };
+      }
+
+      return category;
+    });
+  }
+
+  // Function to chunk categories into rows
+  function chunkCategoriesIntoRows(categories: any[], columns: number): any[] {
+    const rows: any[] = [];
+    for (let i = 0; i < categories.length; i += columns) {
+      rows.push(categories.slice(i, i + columns));
+    }
+    return rows;
   }
 </script>
-
 <div class="flex flex-col gap-5">
   <section class="relative flex justify-center items-center">
     <div class="w-11/12 md:w-4/5 h-[24rem] md:h-[40rem] max-h-screen max-w-5xl mx-auto relative">
@@ -41,7 +63,7 @@
             <a href="/leistungen?tab={category.name}" class="flex flex-col items-center text-center w-60">
               <div class="flex flex-col items-center text-center w-60">
                 <div class="text-lg font-bold border-b-primary pb-1 underline decoration-primary decoration-2 underline-offset-4">{category.name}</div>
-                <img class="w-full h-40 object-cover rounded-md mt-2" src="https://picsum.photos/150" alt={category.name} />
+                <img class="w-full h-40 object-cover rounded-md mt-2" src="{category.image.url}" alt={category.name} />
               </div>
             </a>
           {/each}
