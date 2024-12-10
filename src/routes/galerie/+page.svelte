6e236e59a8be795f77a19f62f6698dcf1fb6d16f
Ralf Pauli <ralf.pauli15@gmail.com>
2024-12-10 22:12:23 +0100

    Adjust Galerie for mobile

--- src/routes/galerie/+page.svelte
+++ src/routes/galerie/+page.svelte
@@ -14,9 +14,11 @@
 </script>
 
 <div class="max-w-5xl mx-auto flex gap-20">
-  <div class="columns-3 gap-5">
+  <div class="columns-1 md:columns-3 gap-5">
     {#each allImages as image}
-      <img class="mb-5" src={image.src} alt={image.alt} />
+      <div class="p-3">
+        <img class="mb-5 rounded-xl" src={image.src} alt={image.alt} />
+      </div>
     {/each}
   </div>
 </div>
